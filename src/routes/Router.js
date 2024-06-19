3b80c963c36c4d75a62d2b5409b8d96e6f5cb398
swayam verma <61063154+swayamverma412@users.noreply.github.com>
2024-06-19 05:42:50 +0530

    changes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,44 +1,44 @@
 import { lazy } from "react";
 import { Navigate } from "react-router-dom";
+import Home from "../Component/pages/home.js"
+import Contactus from "../Component/pages/ContactUs.js";
+import Login from "../Component/pages/Login.js"
+
 
 /**** Layouts *****/
-const FullLayout = lazy(() => import("../layouts/FullLayout"));
+const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
 
 /***** Pages ****/
-const Starter = lazy(() => import("../views/Starter"));
-const About = lazy(() => import("../views/About"));
+const Starter = lazy(() => import("../views/Starter.js"));
+const About = lazy(() => import("../views/About.js"));
 const Cards = lazy(() => import("../views/ui/Cards"));
 const Tables = lazy(() => import("../views/ui/Tables"));
 const Forms = lazy(() => import("../views/ui/Forms"));
-const Monthly = lazy(() => import("../views/ui/Monthly_Report"));
-const Contact = lazy(() => import("../views/ui/ContactUs"));
+const Monthly = lazy(() => import ("../views/ui/Monthly_Report.js"));
+const Contact = lazy(() => import("../views/ui/ContactUs"))
 
-// Separate pages
-const Home = lazy(() => import("../Component/pages/home"));
-const ContactUsPage = lazy(() => import("../Component/pages/ContactUs"));
-const LoginPage = lazy(() => import("../Component/pages/Login"));
-
+// Separate pages 
 /***** Routes *****/
 const ThemeRoutes = [
   {
     element: <FullLayout />,
     children: [
-      { path: "/starter", element: <Starter /> },
-      { path: "/monthly", element: <Monthly /> },
-      { path: "/contactus", element: <Contact /> },
-      { path: "/about", element: <About /> },
-      { path: "/cards", element: <Cards /> },
-      { path: "/table", element: <Tables /> },
-      { path: "/forms", element: <Forms /> },
+      { path: "/starter", exact: true, element: <Starter /> },
+      { path: "/monthly", exact: true, element: <Monthly /> },
+      { path: "/Contactus", exact: true, element: <Contact/> },
+      { path: "/about", exact: true, element: <About /> },
+      { path: "/cards", exact: true, element: <Cards /> },
+      { path: "/table", exact: true, element: <Tables /> },
+      { path: "/forms", exact: true, element: <Forms /> },
     ],
   },
-  {
-    children: [
-      { path: "/", element: <Navigate to="/home" replace /> },
-      { path: "/home", element: <Home /> },
-      { path: "/login", element: <LoginPage /> },
-      { path: "/contact", element: <ContactUsPage /> },
-    ],
+  {path: "/",
+   children: [
+  { path: "/", element: <Navigate to="/home" /> },
+  { path: "/home", exact: true, element: <Home /> },
+  { path: "/login", exact: true, element: <Login /> },
+  { path: "/Contact", exact: true, element: <Contactus /> },
+   ],
   },
 ];
 
